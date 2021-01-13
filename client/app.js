6ddd491d0f448cc3fbe95fc56dd15ff143074bac
Ray Nimalan <raymondnimalan@gmail.com>
2021-01-13 11:46:01 -0500

    feat(allCategoriescComp): added all categories component to homepage

--- client/app.js
+++ client/app.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
 import {Navbar} from './components'
 import Routes from './routes'
+import AllCategories from './components/all-catergories'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <AllCategories />
     </div>
   )
 }
